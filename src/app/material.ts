928ea2c46313287465806f8226c6569c01635ccd
cvillar3989 <cristianjosevillartimana@outlook.com>
2020-05-14 18:20:38 +0200

    commit initial

---
+++ src/app/material.ts
@@ -0,0 +1,78 @@
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatTabsModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatCardModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatDialogModule,
+  MatSelectModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonToggleModule,
+  MatChipsModule,
+  MatDividerModule,
+  MatTreeModule,
+  MatTooltipModule,
+  MatSidenavModule,
+  MatRippleModule,
+  MatGridListModule,
+} from '@angular/material';
+
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTabsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatDividerModule,
+    MatTreeModule,
+    MatTooltipModule,
+    MatSidenavModule,
+    MatRippleModule,
+    MatGridListModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTabsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatDividerModule,
+    MatTreeModule,
+    MatTooltipModule,
+    MatSidenavModule,
+    MatRippleModule,
+    MatGridListModule
+  ]
+})
+
+export class MaterialModule {
+
+}
